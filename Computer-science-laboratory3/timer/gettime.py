a2125768fd0240dd5549d8b1fb51a3d39eb048d4
potyama <kamikaze117117@gmail.com>
2021-06-24 17:27:49 +0900

    雛形

--- Computer-science-laboratory3/timer/gettime.py
+++ Computer-science-laboratory3/timer/gettime.py
@@ -1,10 +1,41 @@
 import serial
 import time
+class getData(object):
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.1)
-time.sleep(2)
-String_data = ser.read()
-print(String_data)
-int_data = int.from_bytes(String_data , 'big')
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        self.val = 0
+        self.start = time.perf_counter()
 
-ser.close()
+
+    def get_data(self):
+        self.val = self.ser.readline().strip()
+        self.val = float(self.val)
+
+        return self.val
+
+    def ser_close(self):
+        self.ser_close()
+
+    def timer(self):
+        self.end = time.perf_counter()
+        print("timer:\n")
+        print(self.end-self.start)
+
+if __name__ == '__main__':
+    data = getData()
+    while True:
+        try:
+            val = data.get_data()
+            if(val < 200):
+                data.ser.close()
+                data.timer()
+                break
+            else:
+                print("val: {}".format(val))
+        except ValueError:
+            pass
+        except KeyboardInterrupt:
+            data.ser.close()
+            data.timer()
+            break
