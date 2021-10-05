483607cec135080b0d00d19a4d5c8a3cb97769bb
potyama <kamikaze117117@gmail.com>
2021-10-05 15:07:08 +0900

    wip

--- Computer-science-laboratory2/b-1/SIR.py
+++ Computer-science-laboratory2/b-1/SIR.py
@@ -22,25 +22,26 @@ def population(t, N):
     fN = np.zeros_like(N)
     gamma = 0.03
     beta = 0.001
-    N = N[0] + N[1] + N[2]
+    alpha = 300
 
-    fN[0] = -(gamma/N)*N[0]*N[1]
-    fN[1] = (gamma/N)*N[0]*N[1] - beta*N[2]
+    fN[0] = -(gamma/(N[0] + N[1] + N[2]))*N[0]*N[1]
+    fN[1] = (gamma/(N[0] + N[1] + N[2]))*N[0]*N[1] - beta*N[2]
     fN[2] = beta * N[1]
     return fN
 
 
 # 0: 人口 1: S 2: I 3:R
 N0 = np.zeros(3)
-N0[0] = 5500    # [入力]:人口の初期値(1920年の人口を万人で入力。10万人なら10と入力)
+N0[0] = 13000    # [入力]:人口の初期値(1920年の人口を万人で入力。10万人なら10と入力)
 N0[1] = 33
 N0[2] = 0
 begin_year = 1920
-end_year = 2008
+end_year = 3000
 
 t, N = euler(N0, 0.1, begin_year, end_year, population)  # 前進オイラー法で解く
 
-plt.plot(t, N)  # 図の描画
+plt.plot(t, N, label="S")
+# 図の描画
 plt.xlabel('Year')
 plt.ylabel('Population [* 10,000]')
 plt.show()  # 図の描画
